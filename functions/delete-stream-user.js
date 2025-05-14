c07c95522f42673122857a16450a37f55bb23adc
Ahmad Nasir <an7539661@gmail.com>
2025-05-15 01:03:58 +0500

    []changes are being made

--- functions/delete-stream-user.js
+++ functions/delete-stream-user.js
@@ -1,8 +1,8 @@
 const { StreamChat } = require('stream-chat');
 const { createClient } = require('@supabase/supabase-js');
 
-const serverClient = StreamChat.getInstance(process.env.STREAM_KEY, process.env.STREAM_SECRET);
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
+const serverClient = StreamChat.getInstance("zcca3ccmptpd", "wxgxh249uxyjye7h9d62geewkrv93gmjxk937cw4fyexycxp57jggg7bjhfcs88m");
+const supabase = createClient("https://dvbckljvgbwinoyrqefw.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2YmNrbGp2Z2J3aW5veXJxZWZ3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NTUxOTAzMCwiZXhwIjoyMDYxMDk1MDMwfQ.pjqsFHf1t1V-wM90l01cvegVc80a5hrprprpnC24spk");
 
 module.exports = async (req, res) => {
   if (req.method !== 'DELETE') return res.status(405).send('Only DELETE allowed');
